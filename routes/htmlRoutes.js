5c0301609afa50423fbef6fa26e887e828319c36
omickelsen <omickelsen@gmail.com>
2019-05-15 21:32:22 -0600

    added the functionality for handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ const path = require(`path`);
 //=========================================
 module.exports = function(app){
 app.get(`/`, function(req, res){
-    res.sendFile(path.join(__dirname, `../public/index.html`));
+   res.render(`index`);
 
 });
 
