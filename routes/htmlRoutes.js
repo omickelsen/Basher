ffd8481e173310eee6e0bb701e107ebc4bc212bc
trome23 <romennoodles23@yahoo.com>
2019-05-21 18:58:29 -0600

    can view all or a limited amount of the database on html page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,21 @@
 //dependencies
-
+var db = require("../models");
 const path = require(`path`);
 
 //routes
 //=========================================
 module.exports = function(app){
 app.get(`/`, function(req, res){
-   res.render(`index`);
-
+   db.bashingTable.findAll({limit:30})
+   // use promise method to pass the burgers...
+   .then(function(basherDB) {
+     console.log(basherDB);
+     // into the main index, updating the page
+     var hbsObject = { bashingTable: basherDB };
+     return res.render("index", hbsObject);
+     
+   })
+   
 });
 
 };
