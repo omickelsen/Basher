cc83cef104ebb4349116f166583978cc84bbd506
Brado <bradenflory2015@gmail.com>
2019-05-29 19:34:19 -0600

    Created the timestamp and added some CSS to get it seperated from the email

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,35 @@
 //dependencies
+var moment = require('moment')
 var db = require("../models");
 const path = require(`path`);
 
 //routes
 //=========================================
-module.exports = function(app){
-app.get(`/`, function(req, res){
-   db.bashingTable.findAll({order: [["createdAt","DESC"]], limit:20})
-   // use promise method to pass the burgers...
-   .then(function(basherDB) {
-     console.log(basherDB);
-     // into the main index, updating the page
-     var hbsObject = { bashingTable: basherDB };
-     return res.render("index", hbsObject);
-     
-   })
-   
-});
+module.exports = function (app) {
+  app.get(`/`, function (req, res) {
+    db.bashingTable.findAll({
+        order: [
+          ["createdAt", "DESC"]
+        ],
+        limit: 20
+      })
+      // use promise method to pass the burgers...
+      .then(function (basherDB) {
+        console.log(basherDB);
+
+        for (let i = 0; i < basherDB.length; i++) {
+          basherDB[i].dataValues.createdAt = moment(basherDB[i].dataValues.createdAt).format("LLL");
+        }
+
+
+        // into the main index, updating the page
+        var hbsObject = {
+          bashingTable: basherDB
+        };
+        return res.render("index", hbsObject);
+
+      })
+
+  });
 
 };
